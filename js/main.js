b1a3a27a9afc03a33fe56a5cb871bfe8bd2cdf3c
coldssslayer <duane@localhost.localdomain>
2024-08-25 10:05:47 -0700

    css errors fixed website has loaded sidebar, header, and footer

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 function loadTemplate(templateId, templateUrl) {
-    fetch(templateUrl)
+  fetch(templateUrl)
       .then(response => response.text())
       .then(data => {
-        document.getElementById(templateId).innerHTML = data;
-      });
-  }
-  
-  window.onload = function() {
-    loadTemplate('header', 'templates/header.html');
-    loadTemplate('footer', 'templates/footer.html');
-  };
-  
+          document.getElementById(templateId).innerHTML = data;
+      })
+      .catch(error => console.error("Error loading template:", error));
+}
+
+window.onload = function() {
+  loadTemplate('header', 'templates/header.html');
+  loadTemplate('footer', 'templates/footer.html');
+  loadTemplate('sidebar', 'templates/sidebar.html'); 
+};
